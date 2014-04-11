33a43b0d3eeb8a5d7112bdd5091bcb49e801a394
Phillip Marfoglio <dcphillip@gmail.com>
2014-04-11 12:39:15 -0500

    implement order quantities

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,9 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
-  validates_presence_of :order_id, :product_id
+  
+  validates :order_id, :product_id, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 
   def subtotal
     quantity * product.price
